3783194e0645c2a0a38d809055d092c843b35b36
jasterel <jasterelafis777@gmail.com>
2024-11-25 17:27:41 +0500

    hswwz

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Аналог __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/proj-vite-v3/',
   plugins: [vue()],
   resolve: {
     alias: {
